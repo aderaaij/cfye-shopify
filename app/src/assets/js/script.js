f9100ebd470df2d01dc4640dfc85c989485f18d9
Arden de Raaij <a.de.raaij@gmail.com>
2015-06-26 18:06:31 +0200

    no updates

--- app/src/assets/js/script.js
+++ app/src/assets/js/script.js
@@ -1,37 +1,5 @@
 jQuery(document).ready(function($) {
-    $('select').selectric({
-        maxHeight: 200
-    });
-
-    $('#search-toggle').on('click', function() {
-        if(!$('.site-header__search-form--active').hasClass('site-header__search-form--active')) {
-            $('.site-header__search-form').addClass('site-header__search-form--active');
-            $('.site-header__search-toggle-icon').toggleClass('site-header__search-toggle-icon--active');
-            $('#search-toggle').addClass('active');
-        } else {
-            $('.site-header__search-form').removeClass('site-header__search-form--active');
-            $('.site-header__search-toggle-icon').toggleClass('site-header__search-toggle-icon--active');
-            $('#search-toggle').removeClass('active');
-        }
-    });
-
-
-  $('.swatch :radio').change(function() {
-    var optionIndex = $(this).closest('.swatch').attr('data-option-index');
-    console.log(optionIndex);
-    var optionValue = $(this).val();
-    console.log(optionValue);
-
-    var productForm = $(this).closest('form');
-    console.log(
-    $(this)
-      .closest('form')
-      .find('.single-option-selector')
-      .eq(optionIndex)
-      .val(optionValue)
-      .trigger('change')
-      );
-  });
-
+    // $('#product-select').selectivizr();
 
 });
+
