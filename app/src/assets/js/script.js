ea2fcb63b8eb5e10c9f890d20615dca5f5533e2f
Arden de Raaij <a.de.raaij@gmail.com>
2015-06-26 20:20:47 +0200

    theme updates

--- app/src/assets/js/script.js
+++ app/src/assets/js/script.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function($) {
-    // $('#product-select').selectivizr();
 
+    // Change swatch
+    $('.swatch :radio').change(function() {
+        var optionIndex = $(this).closest('.swatch').attr('data-option-index');
+        var optionValue = $(this).val();
+        var productForm = $(this).closest('form');
+        $(this).closest('form').find('.single-option-selector').eq(optionIndex).val(optionValue).trigger('change');
+    });
 });
-
