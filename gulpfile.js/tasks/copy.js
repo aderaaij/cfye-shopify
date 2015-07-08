7291dfd5463b234d8ec8dff40d5f905c7042fcb5
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-08 21:30:40 +0200

    carts updates

--- gulpfile.js/tasks/copy.js
+++ gulpfile.js/tasks/copy.js
@@ -8,14 +8,17 @@ gulp.task('copy', function() {
 
   // fonts and font icons
   gulp.src(config.font_src)
+  .pipe(plugins.changed(config.font_build))
   .pipe(plugins.flatten())
   .pipe(gulp.dest(config.font_build));
 
   // Shopify config files
   gulp.src(config.config_src)
+  .pipe(plugins.changed(config.config_build))
   .pipe(gulp.dest(config.config_build));
 
   // Untouched header scripts
   gulp.src(config.script_src)
+  .pipe(plugins.changed(config.script_build))
   .pipe(gulp.dest(config.script_build));
 });
