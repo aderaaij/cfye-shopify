f81d8498fdc456b97037d14025c9b92c346f9dd0
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-12 17:40:10 +0200

    updated clean task

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -1,9 +1,9 @@
 var
     gulp                = require('gulp'),
     del                 = require('del'),
-    config              = require('../config').clean;
+    config              = require('../config/index');
 
 // Clean out build
 gulp.task('clean', function(cb) {
-  del(config.build, cb);
+  del(config.dest, cb);
 });
