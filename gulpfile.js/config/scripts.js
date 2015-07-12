ce68d024aee402f19c6fc793fb8abadfdfedfd3d
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-12 17:50:59 +0200

    updated scripts task

--- gulpfile.js/config/scripts.js
+++ gulpfile.js/config/scripts.js
@@ -7,5 +7,9 @@ module.exports = {
     config.sourceAssets+'js/*.js'
   ],
   dest: config.destAssets,
-  standalone: config.sourceAssets+'js/standalone/**/*{.js,.liquid}'
+  standalone: config.sourceAssets+'js/standalone/**/*{.js,.liquid}',
+  concat: 'scripts.js',
+  rename: {
+    suffix: '.min'
+  }
 }
--- gulpfile.js/tasks/scripts.js
+++ gulpfile.js/tasks/scripts.js
@@ -1,34 +1,31 @@
 var
-    gulp                = require('gulp'),
-    plugins             = require('gulp-load-plugins')(),
-    config              = require('../config').scripts;
-
-// Don't break watch on error
-var onError = function (err) {
-  plugins.util.beep();
-  console.log(err);
-  this.emit('end');
-};
+  gulp                = require('gulp'),
+  plugins             = require('gulp-load-plugins')(),
+  config              = require('../config/scripts'),
+  errorHandler        = require('../lib/errorHandler');
 
 // Scripts
 gulp.task('scripts', function() {
   return gulp.src(config.source)
 
-    // Catch Errors
-    .pipe(plugins.plumber({errorHandler: onError}))
+  // Concatinate in one file
+  .pipe(plugins.concat(config.concat))
 
-    // Concatinate in one file
-    .pipe(plugins.concat('script.js'))
+  // Catch errors
+  .on('error', errorHandler)
 
-    // Add a .min version
-    .pipe(plugins.rename({ suffix: '.min' }))
+  // Add a .min version
+  .pipe(plugins.rename(config.rename))
 
-    // Minify with jsUglify
-    .pipe(plugins.uglify())
+  // Minify with jsUglify
+  .pipe(plugins.uglify())
 
-    // Distribute to build
-    .pipe(gulp.dest(config.build))
+  // Catch errors
+  .on('error', errorHandler)
 
-    // Show notifcation
-    .pipe(plugins.if(global.isWatching, plugins.notify({ message: 'Scripts task complete' })));
+  // Distribute to build
+  .pipe(gulp.dest(config.dest))
+
+  // Show notifcation
+  .pipe(plugins.if(global.isWatching, plugins.notify({ message: 'Scripts task complete' })));
 });
