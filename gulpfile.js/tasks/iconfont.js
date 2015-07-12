499355c3f06d587b47bd0e8a815b824a0d480651
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-12 18:06:51 +0200

    iconfont task update

--- gulpfile.js/tasks/iconfont.js
+++ gulpfile.js/tasks/iconfont.js
@@ -1,30 +1,30 @@
 var
-    gulp                = require('gulp'),
-    plugins             = require('gulp-load-plugins')(),
-    config              = require('../config').iconfont;
+  gulp                = require('gulp'),
+  plugins             = require('gulp-load-plugins')(),
+  config              = require('../config/iconfont');
 
 // Copy files
 gulp.task('iconfont', function() {
 
   // fonts and font icons
-  gulp.src(config.font_src)
+  gulp.src(config.source)
 
   // Only if changed
-  .pipe(plugins.changed(config.font_build))
+  .pipe(plugins.changed(config.dest))
 
   // Flatten folder structure
   .pipe(plugins.flatten())
 
   // Distribute
-  .pipe(gulp.dest(config.font_build));
+  .pipe(gulp.dest(config.dest));
 
   // Start iconfont style.css replace task
-  gulp.src(config.font_style)
+  gulp.src(config.style)
 
   // Replace reference to 'fonts/' with nothing
   .pipe(plugins.replace('fonts/', ''))
 
   // Distribute
-  .pipe(gulp.dest(config.font_build));
+  .pipe(gulp.dest(config.dest));
 
 });
