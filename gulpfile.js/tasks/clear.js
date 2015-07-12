7af2f107e79af33a9e8f8ce76cc00966d9ba7c9f
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-12 17:40:23 +0200

    delete clear task

--- gulpfile.js/tasks/clear.js
+++
@@ -1,8 +0,0 @@
-var
-    gulp                = require('gulp'),
-    plugins             = require('gulp-load-plugins')();
-
-// Clear (image) cache
-gulp.task('clear', function (done) {
-    return plugins.cache.clearAll(done);
-});
