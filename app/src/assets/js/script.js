13b5a77f60b9e208777cf5a0980465c59e936b10
Arden de Raaij <a.de.raaij@gmail.com>
2015-07-19 21:04:28 +0200

    faq

--- app/src/assets/js/script.js
+++ app/src/assets/js/script.js
@@ -23,4 +23,47 @@ jQuery(document).ready(function($) {
 
   // selectric
   $('.shippingCalculator__select').selectric();
+
+  var faqAnswer = $('.faqContent > h2').nextUntil($('.faqContent > h2'));
+  console.log(faqAnswer);
+
+  $('.faqContent > h2').each(function() {
+     $(this).nextUntil('h2').wrapAll('<div class="faqContent__answer"></div>');
+
+    $(this).on('click', function() {
+      // $('.faqContent__answer').removeClass('isActive');
+      if(!$(this).next().hasClass('isActive')) {
+        $(this).next().addClass('isActive');
+      } else {
+        $(this).next().removeClass('isActive');
+      }
+    });
+  });
+  // faqAnswer.each(function() {
+  //   $(this).wrapAll('<div class="bla"></div>');
+  // })
+
+  // $('.faqContent > h2').each(function() {
+  //   var faqAnswer = $('.faqContent > h2').nextUntil($('.faqContent > h2'));
+  //   // faqAnswer.addClass('isHidden');
+
+
+  //   $(this).on('click', function() {
+
+
+  //     // faqAnswer.addClass('isHidden');
+
+  //     // $(this).nextUntil($(this)).removeClass('isHidden');
+  //     if($(this).nextUntil('.faqContent > h2').hasClass('isHidden')) {
+  //       $(this).nextUntil('.faqContent > h2').removeClass('isHidden');
+  //     } else {
+  //       $(this).nextUntil('.faqContent > h2').addClass('isHidden');
+  //     }
+
+
+  //   });
+
+  // });
+
+  // $('.faqContent > h2').nextUntil(".faqContent > h2").hide();
 });
